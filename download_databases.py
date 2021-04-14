0891854d1416f5edf4d6a71199c084f614928e14
VanDeerFrag <74994682+VanDeerFrag@users.noreply.github.com>
2021-04-14 15:35:07 +0200

    Add files via upload

---
+++ download_databases.py
@@ -0,0 +1,51 @@
+import os
+import requests
+from tqdm import tqdm
+
+
+
+
+
+
+def get_confirm_token(response):                                      
+    for key, value in response.cookies.items():                       
+        if key.startswith('download_warning'):                        
+            return value                                              
+                                                                      
+    return None                                                       
+                                                                      
+                                                                    
+def save_response_content(response, destination):                     
+    CHUNK_SIZE = 3000                                                
+                                                                      
+    with open(destination, "wb") as f:                                
+        for chunk in tqdm(response.iter_content(CHUNK_SIZE)):         
+            if chunk: # filter out keep-alive new chunks              
+                f.write(chunk)                                        
+                                                                      
+                                                                      
+def download_file_from_google_drive(id, destination):                 
+    URL = "https://docs.google.com/uc?export=download"                
+                                                                      
+    session = requests.Session()                                      
+                                                                      
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)                               
+                                                                      
+    if token:                                                         
+        params = { 'id' : id, 'confirm' : token }                     
+        response = session.get(URL, params = params, stream = True)   
+                                                                      
+    save_response_content(response, destination)
+
+
+
+
+if __name__ == "__main__":
+
+    if not os.path.isfile("books.csv"):
+        download_file_from_google_drive("1YnXO0GeZ_AwZ8XIY8tsQ-oyXH7RWhrjn", "books.csv")
+
+    if not os.path.isfile("ratings.csv"):
+        download_file_from_google_drive("1v_Xl9n3J4eHGPs5ZIyNyrZIvgVDDqlf1", "ratings.csv")
+        
